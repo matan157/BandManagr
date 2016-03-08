e73775114e05f2bcdebd1ce720b498c702e05427
Matan Uchen <matan.uchen@gmail.com>
2016-03-08 18:29:04 -0500

    base for project

---
+++ src/BandManagr/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BandManagr.ViewModels.Account;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace BandManagr.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/BandManagr/wwwroot/js/login.js
@@ -0,0 +1,31 @@
+﻿// Javascript for Log In page
+
+// returns random integer
+// min = included, max = excluded
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+(function() {
+
+    // array of background images
+    var bgImages = [
+        "bg-1.jpg",
+        "bg-2.jpg",
+        "bg-3.jpg"
+    ];
+
+    // random index for the array
+    var rand = getRandomInt(0, bgImages.length);
+
+    if ($("#login-page").length > 0) {
+        $("body").css({
+            "background": "linear-gradient(" + // linear gradient to create an overlay
+                "rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)" + // black to black with a 45% opacity
+                ")," +
+                "url(../images/login/" + bgImages[rand] + ")",
+            "background-size": "cover",
+            "color": "white"
+        });
+    }
+})();
