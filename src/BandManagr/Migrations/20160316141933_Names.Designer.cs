9576d5305ac2bbca84cce94518734ade24a91bed
Matan Uchen <matan.uchen@gmail.com>
2016-03-16 10:21:10 -0400

    updated user

--- src/BandManagr/Migrations/20160316141933_Names.Designer.cs
+++ src/BandManagr/Migrations/20160316141933_Names.Designer.cs
@@ -8,9 +8,10 @@ using BandManagr.Models;
 namespace BandManagr.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160316141933_Names")]
+    partial class Names
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
